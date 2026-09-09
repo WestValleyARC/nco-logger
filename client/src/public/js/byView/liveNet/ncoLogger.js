d6f940e368d8bffe55d7772a158584c7e93ca32e
ke7wil-bridge <ke7wil@gmail.com>
2026-09-09 12:16:51 -0700

    Match desktop controls height to checked out

--- client/src/public/js/byView/liveNet/ncoLogger.js
+++ client/src/public/js/byView/liveNet/ncoLogger.js
@@ -4067,7 +4067,7 @@ import {
       module.style.setProperty("--nch-module-columns", String(item.w));
       if (id === "controls" && currentLayoutContext === "desktop") {
         module.style.width = "342px";
-        module.style.height = "126px";
+        module.style.height = "120px";
         module.style.justifySelf = "center";
         module.style.alignSelf = "start";
       } else {
