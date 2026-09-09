5c426a169088c1e6e982b676a8bf49da983f4785
ke7wil-bridge <ke7wil@gmail.com>
2026-09-09 14:36:39 -0700

    Match desktop default to approved layout

--- client/dist/public/js/byView/liveNet/ncoLogger.js
+++ client/dist/public/js/byView/liveNet/ncoLogger.js
@@ -88,8 +88,8 @@ import { classifyLoggerLayout, isCurrentResponsiveLayout, isSameLoggerModuleLayo
             lurkers: { x: 0, y: 0, w: 9, h: 4 },
             controls: { x: 9, y: 0, w: 6, h: 3 },
             checkedOut: { x: 15, y: 0, w: 9, h: 4 },
-            chat: { x: 0, y: 4, w: 9, h: 16 },
-            active: { x: 9, y: 4, w: 15, h: 16 }
+            chat: { x: 0, y: 4, w: 8, h: 16 },
+            active: { x: 8, y: 4, w: 16, h: 16 }
         },
         collapsed: {}
     });
