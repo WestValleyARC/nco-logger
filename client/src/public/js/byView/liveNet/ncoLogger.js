ebbffec9b39f3068a11f82e3bec9ec8926a05e85
Joshua Cavender <ke7wil@gmail.com>
2026-09-11 10:55:21 -0700

    Fix Station Controls on short screens

--- client/src/public/js/byView/liveNet/ncoLogger.js
+++ client/src/public/js/byView/liveNet/ncoLogger.js
@@ -66,7 +66,7 @@ import {
     }
     return commands;
   })();
-  const MIN_MODULE_ROWS = Object.freeze({ controls: 3, chat: 5, checkedOut: 2, active: 4, lurkers: 2 });
+  const MIN_MODULE_ROWS = Object.freeze({ controls: 4, chat: 5, checkedOut: 2, active: 4, lurkers: 2 });
   const PREVIOUS_DESKTOP_DEFAULT_MODULE_LAYOUTS = Object.freeze([
     Object.freeze({
       gridVersion: LAYOUT_GRID_VERSION,
@@ -96,11 +96,11 @@ import {
   const DEFAULT_MODULE_LAYOUT = Object.freeze({
     gridVersion: LAYOUT_GRID_VERSION,
     items: {
-      lurkers: { x: 0, y: 0, w: 9, h: 3 },
-      controls: { x: 9, y: 0, w: 6, h: 3 },
-      checkedOut: { x: 15, y: 0, w: 9, h: 3 },
-      chat: { x: 0, y: 3, w: 9, h: 17 },
-      active: { x: 9, y: 3, w: 15, h: 17 }
+      lurkers: { x: 0, y: 0, w: 9, h: 4 },
+      controls: { x: 9, y: 0, w: 6, h: 4 },
+      checkedOut: { x: 15, y: 0, w: 9, h: 4 },
+      chat: { x: 0, y: 4, w: 9, h: 16 },
+      active: { x: 9, y: 4, w: 15, h: 16 }
     },
     collapsed: {}
   });
